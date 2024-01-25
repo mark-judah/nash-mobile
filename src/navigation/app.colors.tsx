9dc392695e4bf0781ed1b095ad8796923b0e019c
allen <allenmuhani@gmail.com>
2024-01-25 13:10:51 +0300

    added screen

--- src/navigation/app.colors.tsx
+++ src/navigation/app.colors.tsx
@@ -2,14 +2,9 @@
  * Contains the list of colors used within the app.
  */
 export enum AppColors {
-    green = '#0d4122',
-    light_green = '#1d7c45',
-    gray = '#eaeaea',
-    yellow = '#ffc71f',
-    black = '#171b22',
-    dark_blue = '#303462',
-    brown = '#6f665e',
-    red = '#E63B2E',
-    perfumeHaze = '#f7effa',
-    link = '#4184e4',
+    darkblue = '#0E2954',
+    torques = '#1B6B93',
+    light_blue = '#51C1F9',
+    red = '#F15A59',
+    background = '#F3F8FF'
 }
